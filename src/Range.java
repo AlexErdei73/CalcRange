7a2e676cafb234002c78b3887c4cb5a6e387a019
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-19 01:16:12 +0000

    Use for loop instead of while loop

--- src/Range.java
+++ src/Range.java
@@ -5,11 +5,15 @@ public class Range {
     private double angleInDegrees;
     private double range;
     Range(double speed) {
-        double angle = 0;
+        /* double angle = 0;
         while (angle <= 90) {
             this.calc(speed, angle);
             this.showRange();
             angle += 5;
+        } */
+        for (double angle = 0; angle <= 90; angle += 5) {
+            this.calc(speed, angle);
+            this.showRange();
         }
     }
     public void calc(double speed, double angle) {
